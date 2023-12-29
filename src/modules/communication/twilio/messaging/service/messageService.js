8f7d912816c72e7cb57ae5e2f7b808a170f5c3a8
David Adediji <davidadey000@gmail.com>
2023-12-29 23:14:27 +0100

    Implement twilio messaging service

---
+++ src/modules/communication/twilio/messaging/service/messageService.js
@@ -0,0 +1,15 @@
+// src\modules\communication\twilio\messaging\service\messageService.js
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+const saveMessageToConversation = async (conversationId, sender, content) => {
+  await prisma.message.create({
+    data: {
+      conversationId: conversationId,
+      sender: sender,
+      content: content,
+    },
+  });
+};
+
+export { saveMessageToConversation };
