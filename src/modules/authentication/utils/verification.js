e165a66b55c628a4b405961ab2689c498999a458
David Adediji <davidadey000@gmail.com>
2024-01-03 12:35:56 +0100

    Improve the verification email implementation to use styled html format

--- src/modules/authentication/utils/verification.js
+++ src/modules/authentication/utils/verification.js
@@ -27,7 +27,23 @@ export function sendVerificationCodeToEmail(email, verificationCode) {
         from: process.env.EMAIL_USER,
         to: email,
         subject: 'Verification Code - Erdbeeren',
-        text: `Your verification code is: ${verificationCode}`,
+        html: `
+        <html>
+            <head>
+                <!-- You can still include a <style> tag if you want -->
+            </head>
+            <body style="font-family: Arial, sans-serif; background-color: #f0f0f0; padding: 20px">
+                <div style="max-width: 600px; margin: 0 auto; padding: 20px; background-color: #fff; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
+                    <h2 style="color: #888; font-size: 24px; text-align: center;">Erdbeeren</h2>
+                    <p style="font-size: 18px; font-weight: bold;">Dear User,</p>
+                    <p style="font-size: 24px; color: #4caf50; font-weight: bold;">Your verification code for Erdbeeren is: <span style="color: #333;">${verificationCode}</span></p>
+                    <p style="margin-top: 10px; font-size: 16px;">Thank you for choosing Erdbeeren!</p>
+                    <hr style="border: 1px solid #ddd; margin-top: 20px; margin-bottom: 20px;">
+                    <p style="font-size: 14px; color: #888;">This email was sent by Erdbeeren. Please do not reply to this email.</p>
+                </div>
+            </body>
+        </html>
+        `,
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
