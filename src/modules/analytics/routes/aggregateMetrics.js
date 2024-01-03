9b9597e2ae467177e77668055b579674ba6916b4
David Adediji <davidadey000@gmail.com>
2024-01-03 21:15:46 +0100

    Implement authMiddleware for analytics routes

--- src/modules/analytics/routes/aggregateMetrics.js
+++ src/modules/analytics/routes/aggregateMetrics.js
@@ -1,14 +1,48 @@
 // src\modules\analytics\routes\metrics.js
-import express from 'express';
-import * as metricsController from '../controllers/aggregateMetricsController.js';
+import express from "express";
+import * as metricsController from "../controllers/aggregateMetricsController.js";
+import {
+  authenticateJWT,
+  hasPermission,
+} from "../../authentication/middleware/authMiddleware.js";
+import { ROLES } from "../../authentication/config/roles.js";
 
 const router = express.Router();
 
-router.get('/customer-satisfaction-trend', metricsController.getCustomerSatisfactionTrend);
-router.get('/average-conversation-duration', metricsController.getAverageConversationDuration);
-router.get('/response-time-trend', metricsController.getResponseTimeTrend);
-router.get('/demographic-customer-analysis', metricsController.getDemographicCustomerAnalysis);
-router.get('/high-frequency-customer-identification', metricsController.getHighFrequencyCustomerIdentification);
-router.get('/overall-sentiment-trend', metricsController.getOverallSentimentTrend);
+router.get(
+  "/customer-satisfaction-trend",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  metricsController.getCustomerSatisfactionTrend
+);
+router.get(
+  "/average-conversation-duration",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  metricsController.getAverageConversationDuration
+);
+router.get(
+  "/response-time-trend",
+
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  metricsController.getResponseTimeTrend
+);
+router.get(
+  "/demographic-customer-analysis",
+  metricsController.getDemographicCustomerAnalysis
+);
+router.get(
+  "/high-frequency-customer-identification",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  metricsController.getHighFrequencyCustomerIdentification
+);
+router.get(
+  "/overall-sentiment-trend",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  metricsController.getOverallSentimentTrend
+);
 
 export default router;
--- src/modules/analytics/routes/individualMetrics.js
+++ src/modules/analytics/routes/individualMetrics.js
@@ -1,38 +1,68 @@
 // src\modules\analytics\routes\individualMetrics.js
-import express from 'express';
-import * as individualMetricsController from '../controllers/individualMetricsController.js';
+import express from "express";
+import * as individualMetricsController from "../controllers/individualMetricsController.js";
+import {
+  authenticateJWT,
+  hasPermission,
+} from "../../authentication/middleware/authMiddleware.js";
+import { ROLES } from "../../authentication/config/roles.js";
 
 const router = express.Router();
 
-// Error handling middleware
-router.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'Internal Server Error' });
-});
-
 // Example route for Individual Customer Satisfaction Metrics
-router.get('/:customerId/satisfaction', individualMetricsController.getSatisfactionMetric);
+router.get(
+  "/:customerId/satisfaction",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getSatisfactionMetric
+);
 
 // Example route for Customer-Specific Conversation Duration
-router.get('/:customerId/conversation-duration', individualMetricsController.getConversationDuration);
+router.get(
+  "/:customerId/conversation-duration",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getConversationDuration
+);
 
 // Example route for Response Time Metrics for Individual Customers
-router.get('/:customerId/avg-agent-response-time', individualMetricsController.getAvgAgentResponseTime);
+router.get(
+  "/:customerId/avg-agent-response-time",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getAvgAgentResponseTime
+);
 
-router.get('/:customerId/avg-customer-response-time', individualMetricsController.getAvgCustomerResponseTime);
+router.get(
+  "/:customerId/avg-customer-response-time",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getAvgCustomerResponseTime
+);
 
 // Example route for Customer Profile-Specific Analysis
-router.get('/:customerId/profile', individualMetricsController.getCustomerProfile);
+router.get(
+  "/:customerId/profile",
+  individualMetricsController.getCustomerProfile
+);
 
 // Example route for Frequency of Interactions for Individual Customers
-router.get('/:customerId/frequency-of-interactions', individualMetricsController.getFrequencyOfInteractions);
+router.get(
+  "/:customerId/frequency-of-interactions",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getFrequencyOfInteractions
+);
 
 // Example route for Sentiment Analysis for Individual Customers
-router.get('/:customerId/sentiment-analysis', individualMetricsController.getSentimentAnalysis);
+router.get(
+  "/:customerId/sentiment-analysis",
+  authenticateJWT,
+  hasPermission([ROLES.MINOTOR]),
+  individualMetricsController.getSentimentAnalysis
+);
 
 export default router;
-
-
 
 // // Example route for Customer Feedback Analysis
 // router.get('/:customerId/feedback', (req, res) => {
