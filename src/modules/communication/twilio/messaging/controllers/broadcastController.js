2a433ee37e1ca58e0919f8205416dd3fe7027a85
David Adediji <108187742+davidtadediji@users.noreply.github.com>
2024-06-26 11:52:15 +0400

    Resolve issues with broadcast

--- src/modules/communication/twilio/messaging/controllers/broadcastController.js
+++ src/modules/communication/twilio/messaging/controllers/broadcastController.js
@@ -1,9 +1,9 @@
 // src/controllers/broadcastController.js
-import { PrismaClient } from '@prisma/client';
-import { updateConversationTimestamp } from '../services/conversationService.js';
-import { saveMessageToConversation } from '../services/messageService.js';
-import twilio from 'twilio';
-import logger from "../../../../../../logger.js"
+import { PrismaClient } from "@prisma/client";
+import { updateConversationTimestamp } from "../services/conversationService.js";
+import { saveMessageToConversation } from "../services/messageService.js";
+import twilio from "twilio";
+import logger from "../../../../../../logger.js";
 const prisma = new PrismaClient();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -13,39 +13,39 @@ const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = twilio(accountSid, authToken);
 
 const broadcastMessage = async (content) => {
-  logger.info("Broadcast triggered")
+  logger.info("Broadcast triggered");
   try {
     // Get all conversations
     const conversations = await prisma.conversation.findMany();
 
-    // Send message to each conversation
+    // Send content to each conversation
     await Promise.all(
       conversations.map(async (conversation) => {
-        logger.info("Conversation encountered.")
+        logger.info("Conversation encountered.");
         const formattedPhoneNumber =
-          conversation.type === 'whatsapp'
+          conversation.type === "whatsapp"
             ? `whatsapp: ${twilioPhoneNumber}`
             : twilioPhoneNumber;
 
-      
         // await client.messages.create({
         //   body: content,
         //   from: formattedPhoneNumber,
-        //   to: conversation.participantSid, 
+        //   to: conversation.participantSid,
         // });
 
-       
-        await saveMessageToConversation(conversation.id, 'broadcast', content);
+        await saveMessageToConversation(conversation.id, "broadcast", content);
 
-      
         await updateConversationTimestamp(conversation.id);
       })
     );
 
     return { success: true };
   } catch (error) {
-    logger.error('Error broadcasting message:', error.message);
-    throw error;
+    logger.error("Error broadcasting message:", error.message);
+    // throw error;
+    res
+      .status(500)
+      .json({ error: "Internal server error", message: error.message });
   }
 };
 
