239faa226920c17f3bdbeb10529645f47730ca06
David Adediji <108187742+davidtadediji@users.noreply.github.com>
2024-06-27 17:36:29 +0400

    Implement text based logger

--- src/modules/audit_logger/auditRoutes.js
+++ src/modules/audit_logger/auditRoutes.js
@@ -5,14 +5,16 @@ import {
   hasPermission,
 } from "../authentication/middleware/authMiddleware.js";
 import logger from "../../../logger.js";
+import path from "path";
+import fs from "fs/promises";
 
 const prisma = new PrismaClient();
 const router = express.Router();
 
 router.use(authenticateJWT);
-// router.use(hasPermission(["viewAuditLogs"]));
+router.use(hasPermission(["viewAuditLogs"]));
 
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
   logger.info("Audit trail Triggered");
   try {
     const auditLogs = await prisma.auditLog.findMany({
@@ -26,7 +28,33 @@ router.get("/", async (req, res) => {
     });
     res.json(auditLogs);
   } catch (error) {
-    throw error;
+    next(error);
+  }
+});
+
+const currentModuleURL = new URL(import.meta.url);
+const currentModuleDir = path.dirname(currentModuleURL.pathname);
+
+logger.info("File service dir: " + currentModuleDir);
+
+const LOG_FILE_PATH = path.join(
+  currentModuleDir.replace(/^\/([A-Z]:)/, "$1"),
+  "..",
+  "..",
+  "..",
+  "app.log"
+);
+
+router.get("/text", async (req, res, next) => {
+  logger.info("Get Logs triggered");
+  try {
+    const data = await fs.readFile(LOG_FILE_PATH, "utf8");
+    const lines = data.trim().split("\n");
+    const last500Lines = lines.slice(-500).join("\n");
+    res.setHeader("Content-Type", "text/plain");
+    res.send(last500Lines);
+  } catch (error) {
+    next(error);
   }
 });
 
