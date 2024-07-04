3054f359454e49a8cb436afa3b9be45b47c90b98
David Adediji <108187742+davidtadediji@users.noreply.github.com>
2024-07-04 22:45:07 +0400

    customer profiler seems stable

--- src/modules/llm_context/services/customerProfiler.js
+++ src/modules/llm_context/services/customerProfiler.js
@@ -1,13 +1,16 @@
+// src\modules\llm_context\services\customerProfiler.js
 import OpenAI from "openai";
 
 import dotenv from "dotenv";
 
 dotenv.config();
 
+// Initializing OpenAI with API key from environment variables
 const openai = new OpenAI({
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
+// Define an array of customer profile details to be extracted from a conversation with descriptions of parameters
 const customer_profile_functions = [
   {
     name: "extractCustomerInfo",
@@ -15,6 +18,7 @@ const customer_profile_functions = [
     parameters: {
       type: "object",
       properties: {
+        // extract name, email, phonenumber, preferred mode of contact and purchase history of the customer if stated in the conversation
         name: {
           type: "string",
           description: "Name of the customer",
@@ -53,20 +57,17 @@ const customer_profile_functions = [
           },
           description: "List of previous purchases",
         },
-        customer_agent_satisfaction: {
-          type: "string",
-          description:
-            "User satisfaction on conversation agent's performance, never about product or service directly offered by company",
-        },
       },
     },
   },
 ];
 
-// Function to extract customer profile
+/* Function to extract customer profile information from a given message, this is called by the monitor/admin
+ to generate a customer profile based on customer-related information from a customer's entire conversation. 
+ it is then displayed as part of the report, no sensitive information is collected!*/
 export const extractProfile = async (message) => {
   const prompt = `Check this message: ${message}, to extract any customer detail, must return empty if no info exists, must never add any info non-existent in the message`;
-  const model = "gpt-3.5-turbo-1106";
+  const model = "gpt-4";
   const max_tokens = 500;
   const top_p = 1;
   const frequency_penalty = 0;
@@ -74,6 +75,7 @@ export const extractProfile = async (message) => {
   const temperature = 0;
 
   try {
+    // Request OpenAI to complete the prompt and extract customer information
     const response = await openai.chat.completions.create({
       model,
       messages: [{ role: "assistant", content: prompt }],
@@ -85,17 +87,13 @@ export const extractProfile = async (message) => {
       presence_penalty,
       temperature,
     });
-    console.log(response.choices[0].message.function_call);
+
+    // Carefully extract the json object from OpenAI's response and return it
     const bit = response.choices[0].message.function_call;
     const args = JSON.parse(bit.arguments);
-    console.log(args);
     return args;
   } catch (error) {
     console.error("Error during extraction:", error.message);
-    return "unknown";
+    return "unknown"; // Returning "unknown" as default in case of error
   }
 };
-
-// const message = "Hello";
-// const customerProfile = await extractProfile(message);
-// console.log(customerProfile);
