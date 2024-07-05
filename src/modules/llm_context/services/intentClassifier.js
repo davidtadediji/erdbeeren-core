37dfe2e0a748f2f1907c6090e85b19760f5d8e0e
David Adediji <108187742+davidtadediji@users.noreply.github.com>
2024-07-05 07:56:59 +0400

    Tried to improve entire intent classifer

--- src/modules/llm_context/services/intentClassifier.js
+++ src/modules/llm_context/services/intentClassifier.js
@@ -1,78 +1,34 @@
-import dotenv from "dotenv";
+import dotenv from "dotenv"; // Importing dotenv for environment variables
 import OpenAI from "openai"; // Importing OpenAI API wrapper
 import logger from "../../../../logger.js"; // Importing logger module
 import {
   createTicket,
   selectRandomAgent,
-} from "../../ticketing_system/services/agentTicketService.js"; // Importing agent ticketing system functions
-import { respondToMessage } from "./modelService.js"; // Importing function for llm response.
+} from "../../ticketing_system/services/agentTicketService.js"; // Importing ticketing system functions
+import { respondToMessage } from "./modelService.js"; // Importing local function
 
-dotenv.config();
+dotenv.config(); // This loads environment variables from .env file
 
-// Function to route request based on customer intent
-export const routeRequest = async (
-  message,
-  conversationId,
-  isAgent = false,
-  previousMessages
-) => {
-  // get the message intent classification
-  const classification = await classifyMessage(message);
-
-  // route depending on the classification
-  switch (classification) {
-    case "service request":
-      handleRequireHuman(message, conversationId, "service request");
-      // no response because it would be routed to agent
-      return null;
-    case "service complaint":
-      handleRequireHuman(message, conversationId, "service complaint");
-      // no response because it would be routed to agent
-      return null;
-    case "enquiry":
-      return handleRequireAI(
-        message,
-        conversationId,
-        isAgent,
-        previousMessages,
-        "enquiry"
-      );
-    default:
-      return handleRequireAI(
-        message,
-        conversationId,
-        isAgent,
-        previousMessages,
-        "unknown"
-      );
-  }
-};
-
-const openai = new OpenAI({
-  // This initializes OpenAI with API key from environment variables
+const openai = new OpenAI({ // This initializes OpenAI with API key from environment variables
   apiKey: process.env["OPENAI_API_KEY"],
 });
 
-// Function to classify the message using OpenAI's GPT model, the request router utilizes this function
+// Function to classify the message using OpenAI's GPT-3 model
 const classifyMessage = async (message) => {
-  const prompt = `Classify the following message as either 'service request', 
-  'service complaint', 'enquiry' or 'other' type: ${message}`;
+  const prompt = `Classify the following message as either 'service request', 'service complaint', 'enquiry' or 'other' type: ${message}`;
   const model = "gpt-4"; // This sets the model version
   const max_tokens = 10; // This defines the maximum number of tokens for the completion
-  const top_p = 1; // This sets the sampling parameter for response diversity
-  /* Setting the temperature parameter, frequency penalty and presence penalty
-    for sampling to 0 makes model responses deterministic and conservative. */
-  const frequency_penalty = 0;
-  const presence_penalty = 0;
-  const temperature = 0;
+  const top_p = 1; // This sets the sampling parameter for diversity
+  const frequency_penalty = 0; // This sets the frequency penalty for the model
+  const presence_penalty = 0; // This sets the presence penalty for the model
+  const temperature = 0; // This sets the temperature parameter for sampling
 
   try {
     // This makes an API call to OpenAI for message classification
     const response = await openai.chat.completions.create({
       model,
       messages: [{ role: "assistant", content: prompt }],
-      /*Function calling restricts the model's output to predictable outcomes, 
-      which is beneficial for programming purposes*/
+      // function call rre
       functions: [
         {
           name: "getIntent",
@@ -108,40 +64,102 @@ const classifyMessage = async (message) => {
 
     return intent;
   } catch (error) {
-    logger.error("Error during classification:", error.message);
+    logger.error("Error during classification:", error.message); 
     return "unknown"; /* We handle errors in classification 
     by returning 'unknown'  */
   }
 };
 
-// Function to handle service requests and incident complaints
-const handleRequireHuman = async (message, conversationId, type) => {
-  try {
-    logger.info(`Handling service request: ${message}`);
-    // select a random agent 
-    const agentId = await selectRandomAgent();
-    // create a ticket and assign it to the agent to address the issue
-    await createTicket(agentId, type, conversationId, message);
-  } catch (error) {
-    logger.error(`Error occurred while handling ${type}!`, error);
+export const routeRequest = async (
+  message,
+  conversationId,
+  isAgent = false,
+  previousMessages
+) => {
+  const classification = await classifyMessage(message);
+  // const classification = "enquiry";
+  // const classification = "service request"
+  console.log("Classification: ", classification);
+
+  switch (classification) {
+    case "service request":
+      handleServiceRequest(message, conversationId);
+      return null;
+    case "service complaint":
+      handleServiceComplaint(message, conversationId);
+      return null;
+    case "enquiry":
+      return handleEnquiry(message, conversationId, isAgent, previousMessages);
+    default:
+      return handleUnknown(message, conversationId, isAgent, previousMessages);
   }
 };
 
-// Function to handle enquires and messages of undefined type
-const handleRequireAI = async (
+// Function to handle service requests
+const handleServiceRequest = async (message, conversationId) => {
+  try {
+    logger.info(`Handling service request: ${message}`);
+    const type = "service request";
+    const agentId = await selectRandomAgent();
+    console.log("Random Agent: ", agentId);
+    await createTicket(agentId, type, conversationId, message);
+  } catch (error) {
+    logger.error("Error occurred while handling service request!", error);
+  }
+};
+
+// Function to handle complaints
+const handleServiceComplaint = async (message, conversationId) => {
+  try {
+    logger.info(`Handling service complaint: ${message}`);
+    const type = "service complaint";
+    const agentId = await selectRandomAgent();
+    console.log("Random Agent: ", agentId);
+    await createTicket(agentId, type, conversationId, message);
+  } catch (error) {
+    logger.error("Error occurred while handling service complaint!", error);
+  }
+};
+
+// handleServiceComplaint("Hello", "05f21bc4-6c69-4e0b-9fcf-a46a499bca53");
+
+// Function to handle complaints
+const handleEnquiry = async (
   message,
   conversationId,
   isAgent,
-  previousMessages,
-  type
+  previousMessages
 ) => {
-  logger.info(`Handling ${type}: ${message}`);
-  // pass the message to the LLM to respond
-  const res = await respondToMessage(message, conversationId, isAgent, previousMessages);
+  logger.info(`Handling enquiry: ${message}`);
+  const res = await respondToMessage(
+    message,
+    conversationId,
+    isAgent,
+    previousMessages
+  );
+  // const res = "Hello"
   return res;
 };
 
-// Function to handle poor sentiment, when a customers previous messages have been under a certain sentiment threshold
+// Function to handle unknown request types
+const handleUnknown = async (
+  message,
+  conversationId,
+  isAgent,
+  previousMessages
+) => {
+  logger.info(`Handling unknown: ${message}`);
+  const res = await respondToMessage(
+    message,
+    conversationId,
+    isAgent,
+    previousMessages
+  );
+  // const res = "Hello";
+  return res;
+};
+
+// Function to handle complaints
 export const handlePoorSentiment = async (message, conversationId) => {
   try {
     logger.info(`Handling service complaint: ${message}`);
